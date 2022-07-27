e4bd85bd826e1a126d1ca6319c65ba6e5de2bd3b
catzilla <3qandre17@gmail.com>
2022-07-27 09:16:24 +0700

    develop: Added Home basic Template

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,20 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <div>Home</div>
+    <div>
+      <Typography variant='h2'>
+        about
+      </Typography>
+      Lorem ipsum dolor sit amet
+      consectetur adipisicing elit.
+      Libero debitis id quod repellendus hic dolorem cum,
+      amet optio magnam aperiam, quisquam quibusdam. Quod,
+      molestias? Eaque error quo numquam maxime soluta.
+    </div>
   )
 }
 
