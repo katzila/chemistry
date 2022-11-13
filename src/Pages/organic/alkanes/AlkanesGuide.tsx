ba65a2250395c2b1fa42d59658336c350d057da2
katzila <3qandre17@gmail.com>
2022-11-13 14:26:46 +0600

    develop: Added chemicalProperties, Refac CI AlkanesGuide gaps - Added chemicalProperties - Refac CI AlkanesGuide gaps

--- src/Pages/organic/alkanes/AlkanesGuide.tsx
+++ src/Pages/organic/alkanes/AlkanesGuide.tsx
@@ -1,12 +1,13 @@
 import { Box, Paper, Typography } from '@mui/material';
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 import Izomer1 from '../../../assets/alkanes/1.png'
 import Izomer2 from '../../../assets/alkanes/2.png'
 import Izomer3 from '../../../assets/alkanes/3.png'
 import P1 from '../../../assets/alkanes/p1.png'
 import P2 from '../../../assets/alkanes/p2.png'
-import { useTranslation } from 'react-i18next';
+import alkanesScheme from '../../../assets/alkanes/alkanesScheme.svg'
 
 // import Pentane from '../../../assets/alkanes/пентан.html'
 
@@ -18,17 +19,20 @@ const AlkanesGuide = () => {
       role="tabpanel"
     >
       <Box sx={{ p: 2, display: 'flex' }}>
-        <Box sx={{ width: '50%', pr: 2 }}>
-          <Typography variant='h5'>{t('guide.definitionTitle')}</Typography>
+        <Box sx={{ width: '50%', pr: 2, display: 'flex', flexDirection: 'column', gap: '15px' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            <Typography variant='h5'>{t('guide.definitionTitle')}</Typography>
 
-          <Paper elevation={0} sx={{ p: 1, borderLeft: 'solid 3px #bbbbbb' }}>
-            <p>
-              {t('guide.alkanes.definitionText')}
-            </p>
-            <Typography variant='caption'>
-              {t('guide.alkanes.definitionCredits')}
-            </Typography>
-          </Paper>
+            <Paper elevation={0} sx={{ p: 1, borderLeft: 'solid 3px #bbbbbb' }}>
+              <p>
+                {t('guide.alkanes.definitionText')}
+              </p>
+              <Typography variant='caption'>
+                {t('guide.alkanes.definitionCredits')}
+              </Typography>
+            </Paper>
+          </Box>
+
 
 
           <Typography variant='h5'>{t('guide.empiricalFormula')}</Typography>
@@ -43,12 +47,13 @@ const AlkanesGuide = () => {
           <Typography variant='h5'>{t('guide.isomerism')}</Typography>
 
           <Typography variant='h6'>{t('guide.branchingCarbonSkeleton')}</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            <img src={Izomer1} alt='Izomer1' style={{ width: '315px', maxWidth: '100%' }} />
 
-          <img src={Izomer1} alt='Izomer1' style={{ maxWidth: '315px' }} />
+            <img src={Izomer2} alt='Izomer2' style={{ width: '250px', maxWidth: '100%' }} />
 
-          <img src={Izomer2} alt='Izomer2' style={{ maxWidth: '250px' }} />
-
-          <img src={Izomer3} alt='Izomer3' style={{ maxWidth: '180px' }} />
+            <img src={Izomer3} alt='Izomer3' style={{ width: '180px', maxWidth: '100%' }} />
+          </Box>
 
           <Typography variant='h5'>{t('guide.physicalProperties')}</Typography>
 
@@ -64,11 +69,11 @@ const AlkanesGuide = () => {
           <ol>
             <li>
               <p>{'Гидрирование непредельных углеводородов (t, катализаторы: Ni, Pt, Pd):'}</p>
-              <img src={P1} alt='P1' style={{ maxWidth: '350px' }} />
+              <img src={P1} alt='P1' style={{ width: '350px', maxWidth: '100%' }} />
             </li>
             <li>
               <p>{'На основе "синтез-газа":'}</p>
-              <img src={P2} alt='P2' style={{ maxWidth: '500px' }} />
+              <img src={P2} alt='P2' style={{ width: '500px', maxWidth: '100%' }} />
             </li>
           </ol>
 
@@ -83,9 +88,13 @@ const AlkanesGuide = () => {
             <li>Топливо</li>
           </ul>
         </Box>
-        <Box sx={{ width: '50%', pl: 2 }}>
+        <Box sx={{ width: '50%', pl: 2, textAlign: 'center' }}>
           <iframe title='pentane' src="/пентан.html?autorotate&no_social" width="100%" //temporary
             height="600px" frameBorder="0" style={{ boxShadow: '0 0 10px rgba(0,0,0,0.5)' }} ></iframe>
+          <Typography variant='subtitle1' fontWeight='bold' >{t('guide.alkanes.modelPentan')}</Typography>
+
+          <img src={alkanesScheme} alt="alkanesScheme" style={{ maxWidth: '100%' }} />
+          <Typography variant='subtitle1' fontWeight='bold' >{t('guide.chemicalProperties')}</Typography>
         </Box>
       </Box>
     </div>
